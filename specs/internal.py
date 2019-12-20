b4354ebf25708f86819e41d121e2dc54d2eb95eb
Shacham Ginat <shacham6@gmail.com>
2019-12-20 12:07:54 +0200

    Raise when conforming unsupported type

--- specs/internal.py
+++ specs/internal.py
@@ -4,4 +4,10 @@ from specs import spec_types
 def conform_spec(spec):
     if isinstance(spec, type):
         return spec_types.type_spec(spec)
-    return spec
+    elif isinstance(spec, spec_types.specification):
+        return spec
+    raise UnsupportedSpecification()
+
+
+class UnsupportedSpecification(Exception):
+    pass
--- tests/internal/test_spec_conformation.py
+++ tests/internal/test_spec_conformation.py
@@ -1,4 +1,5 @@
-from specs.internal import conform_spec
+import pytest
+from specs.internal import conform_spec, UnsupportedSpecification
 from specs import is_valid, spec_types
 
 
@@ -7,7 +8,15 @@ def test_conforms_types():
         conform_spec(int),
         spec_types.type_spec,
     )
+
+
+def test_returns_self_when_spec_provided():
     assert isinstance(
         conform_spec(spec_types.either(int, str)),
         spec_types.either,
     )
+
+
+def test_raises_when_provided_unsupported_spec():
+    with pytest.raises(UnsupportedSpecification):
+        conform_spec(22)
